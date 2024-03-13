cf1ef50c3d807f88efd101c84862358a53ac76ee
leonardo <leochinelato9852@gmail.com>
2024-03-13 18:28:49 -0300

    Criar página 'novo' e estilizar com css e bootstrap

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request, flash, session
 
-titulo = 'Dieta'
+
+class Alimento:
+    def __init__(self, nome, quantidade):
+        self.nome = nome
+        self.quantidade = quantidade
+
+
+alimentos = []
 
 app = Flask(__name__)
+app.secret_key = '09UDSADS09Sj)(i*&&)'
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', titulo=titulo)
+    return render_template('index.html', titulo='Dieta', alimentos=alimentos)
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo treino')
+
 
 app.run(debug=True)
